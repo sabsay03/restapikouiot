b29dd5a7073d658df1d651673a3a0d28cb518982
sabsay03 <sabrikusaygulmez@gmail.com>
2021-12-18 17:17:19 +0300

    restapi

---
+++ src/main/java/com/restapikouiot1/restapikouiot1/Restapikouiot1Application.java
@@ -0,0 +1,13 @@
+package com.restapikouiot1.restapikouiot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restapikouiot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restapikouiot1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapikouiot1/restapikouiot1/Restapikouiot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapikouiot1.restapikouiot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restapikouiot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
